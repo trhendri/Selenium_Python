8f342fa7d9fde7409afe53430d5a5c0895c2e626
Talise Hendrix <trhendri1@gmail.com>
2025-04-11 09:32:18 -0500

    Add Pytest and Webscraping practice

--- WebScraping/Mukesh/webscrapingbestbuy.py
+++ WebScraping/Mukesh/webscrapingbestbuy.py
@@ -2,14 +2,16 @@ from selenium import webdriver
 import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import inspect
+from openpyxl import Workbook
+import openpyxl
+
 
 
 driver = webdriver.Chrome()
 driver.maximize_window()
 
 driver.get("https://bestbuy.com")
-driver.implicitly_wait(10)
+driver.implicitly_wait(20)
 
 search_input = driver.find_element(By.ID, "gh-search-input")
 search_input.send_keys("Samsung phones")
@@ -17,29 +19,50 @@ search_input.send_keys(Keys.ENTER)
 samsung_checkbox = driver.find_element(By.ID, "brand_facet-Samsung-0")
 samsung_checkbox.click()
 
-phone_names = driver.find_elements(By.CLASS_NAME, "sku-title")
-phone_prices = driver.find_elements(By.XPATH, "//div[@data-testid=\"customer-price\"]//span[@aria-hidden=\"true\"]")
-first_18_phone_prices = phone_prices[:18]
-
 my_phone=[]
 my_price=[]
 
+# phone_names = driver.find_elements(By.XPATH, "//div[@class=\"column-middle\"]/h4[@class=\"sku-title\"]")
+# # first_18_phone_names = phone_names[:18]
+# phone_prices = driver.find_elements(By.XPATH, "//div[@class='column-right']//div[@data-testid='customer-price']//span[@aria-hidden='true']")
+# # first_18_phone_prices = phone_prices[:18]
+
+
+
+phone_names = driver.find_elements(By.XPATH, "//div[@class=\"column-middle\"]/h4[@class=\"sku-title\"]")
 for phone in phone_names:
-    print(phone.text)
     my_phone.append(phone.text)
 
 print("*" *50)
+phone_prices = driver.find_elements(By.XPATH, "//div[@class='column-right']//div[@data-testid='customer-price']//span[@aria-hidden='true']")
 
-for price in first_18_phone_prices:
-    print(price.text)
+for price in phone_prices:
     my_price.append(price.text)
 
 
-final_list = zip(my_phone,my_price)
+final_list = list(zip(my_phone,my_price))
 
-for data in list(final_list):
+for data in final_list:
     print(data)
 
+print("Part 1: Write to excel file")
+
+
+#create new Excel sheet
+
+wb=Workbook()
+wb['Sheet'].title='BestBuy Samsung Data'
+sh1=wb.active
+
+sh1.append(['Name', 'Price'])
+
+for x in final_list:
+    print("Writing to doc")
+    sh1.append(x)
+
+wb.save("Final_Samsung_Deals.xlsx")
+
+print("Part 2: Send Email")
 
 
 
